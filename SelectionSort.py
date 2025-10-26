7db2036a44f5cfaca823199c615d8f705de73fae
dx ddd <oyunaydash@gmail.com>
2025-10-27 00:08:38 +0700

    Selection Sort SS created

---
+++ SelectionSort.py
@@ -0,0 +1,16 @@
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+def selectionSort(arr):
+    newArr = []
+    for i in range(0, len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+print(selectionSort([32,1,54,3,5,2]))
